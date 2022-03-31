c93bb8defbd6c2d587a7a022725b84daa7b4e77d
Carlos <Carlosrdf45@gmail.com>
2022-03-31 12:03:58 -0400

    Details init

---
+++ client/src/components/DetailsPackage.jsx
@@ -0,0 +1,4 @@
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getDetail, resetDetail } from "../actions";
