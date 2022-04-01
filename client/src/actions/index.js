69bbab9666de09625c98c32568bcc016b9167975
Albamar <albamarf3@gmail.com>
2022-03-31 23:39:58 -0400

    actions-reducer-detail

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,62 @@
-export const SEARCH_DESTINATION = 'SEARCH_DESTINATION';
+
 const axios = require('axios');
 
 
 export const searchDestination = (name) => async (dispatch) => {
     let response = await  axios.get('?name=' + name);
     return dispatch({
-        type: SEARCH_DESTINATION,
+        type: "SEARCH_DESTINATION",
         payload: response.data,
     });
 };
+
+export function getPlains() {
+    return async function (dispatch) {
+        var json = await axios.get('http://localhost:8800/getplains');
+        //console.log(json.data)
+        return dispatch({
+            type: "GET_PLAINS",
+            payload: json.data
+        })
+    }
+};
+
+export function getDetailId(id) {
+    return async function (dispatch) {
+        let response = await axios.get('http://localhost:8800/' + id);
+        console.log('juth', response.data)
+        return dispatch({
+            type: "GET_DETAIL",
+            payload: response.data,
+        });
+    }
+};
+
+
+        // filtrado por provincia
+
+        export function filterByProvince(payload) {
+            return {
+                type: "FILTER_BY_PROVINCE",
+                payload
+            }
+        }
+
+        // Ordenamiento por precio
+
+        export function orderByPrice(payload) {
+            return {
+                type: "ORDER_BY_PRICE",
+                payload
+            }
+        }
+
+        // Ordenamiento por Estrellas o Puntuacion
+
+
+        export function orderByScore(payload) {
+            return {
+                type: "ORDER_BY_SCORE",
+                payload
+            }
+        }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,17 +1,93 @@
-import { SEARCH_DESTINATION } from "../actions";
+
 const initialState = {
-    destination : [],
+    plains : [],
+    plainsCopy : [],
+    destination: [],
+    detail: []
+    
 };
 
 function rootReducer(state = initialState, action){
     switch (action.type) {
-        case SEARCH_DESTINATION:
+        case "GET_PLAINS": 
+        //console.log(action.payload)
+            return {
+                ...state,
+                plains: action.payload,
+                plainsCopy: action.payload
+            }
+        case 'GET_DETAIL':
+            console.log('juth',action.payload)
+                return {
+                    ...state,
+                    detail: action.payload
+                }
+            case "FILTER_BY_PROVINCE":
+                const allPlains = state.plainsCopy
+                const provinceFilter = action.payload === "All" 
+                ? allPlains
+                : allPlains.filter(e=> e.location === action.payload)
+                return {
+                    ...state,
+                    plains: provinceFilter
+                }
+            case "ORDER_BY_PRICE":
+            const sortByPrice = action.payload === "asc"
+            ? state.plains.slice().sort(function(a,b) {
+                if(a.price > b.price) {
+                    return 1;
+                }
+                if(b.price > a.price) {
+                    return -1;
+                }
+                return 0
+            }) 
+            : state.plains.slice().sort(function(a,b) {
+                if(a.price > b.price) {
+                    return -1;
+                }
+                if(b.price > a.price) {
+                    return 1;
+                }
+                return 0
+            })
+            return{
+                ...state,
+                plains : sortByPrice
+            }
+            case "ORDER_BY_SCORE":
+                const sortByScore = action.payload === "asc"
+                ? state.plains.slice().sort(function(a,b) {
+                    if(a.score > b.score) {
+                        return 1;
+                    }
+                    if(b.score > a.score) {
+                        return -1;
+                    }
+                    return 0
+                }) 
+                : state.plains.slice().sort(function(a,b) {
+                    if(a.score > b.score) {
+                        return -1;
+                    }
+                    if(b.score > a.score) {
+                        return 1;
+                    }
+                    return 0
+                })
+                return{
+                    ...state,
+                    plains : sortByScore
+                }
+        case 'SEARCH_DESTINATION':
             return {
                 ...state,
                 destination: action.payload
             };
+            
         default:
             return state;
     }
 };
 export default rootReducer;
+
