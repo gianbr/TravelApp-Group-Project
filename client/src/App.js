07ea3bd91091e64630a3584f28e1c7379ea89c5e
Carlos <Carlosrdf45@gmail.com>
2022-04-01 14:15:58 -0400

    finished Carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 
-import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
-import img1 from "./assets/actividad3.jpeg"
-import Carousel from './components/carousel'
 
 function App() {
   return (
     <div>
-        <Carousel 
-          images={img1}
-        />
+        Hola
     </div>
   );
 };
