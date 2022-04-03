4485c8683c7d9cc3fb5f8d455b23c7ecb20dc7fe
aleconiglio16 <alejandroconiglio16@gmail.com>
2022-04-03 16:25:22 -0300

    details y carrousel

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,47 @@
+
+import React, { useEffect } from 'react'
+import { Carousel } from "react-responsive-carousel";
+import { useParams } from 'react-router-dom'
+import { getDetailId } from '../actions/index'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { useSelector, useDispatch } from 'react-redux';
+
+const Carousel1 = () => {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const detail = useSelector((state) => state.detail);
+
+
+    useEffect(() => {
+        dispatch(getDetailId(id));
+    }, [dispatch, id]);
+
+
+    return (
+        <div>
+            <Carousel
+                useKeyboardArrows
+                infiniteLoop
+                autoPlay
+                interval={6000}
+                showThumbs={false}
+            >
+                {detail.images?.map((e, i) => {
+                    return (
+                        <div className='flex justify-center'>
+                            <div key={i} className="w-8/12">
+                                <img
+                                    className="flex h-96 rounded-sm"
+                                    src={e}
+                                    alt="Not found"
+                                />
+                            </div>
+                        </div>
+                    );
+                })}
+            </Carousel>
+        </div>
+    );
+};
+
+export default Carousel1;
--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import {getDetailId} from '../actions/index'
 import { useSelector,useDispatch} from 'react-redux';
-
+import { Link } from "react-router-dom"
+import Carousel from "./Carousel"
+import CarouselCom from './CarouselCom';
 
 
 function Details (){
@@ -17,29 +19,58 @@ function Details (){
   
 
   return (
-    <div>
-      <h3>{detail.title}</h3>
-      {detail.images?.map((e) => (
-        <img src={e} alt={e} key={e}/>
-      ))} 
-      <h3>{detail.location} </h3>
-      <h3>{detail.city}</h3>
-      <h3>{detail.description}</h3>
-
+    <div>      
+    <div  className='border-2 border-teal-500 mx-28  bg-white'>
+          
+        <div>
+        <h3 className='ml-11 mt-4 font-bold text-3xl'>{detail.city}, {detail.location}</h3>
+        <div>
+          
+          {detail.score<=2
+        ? <h3 className='text-right mr-2'>Puntaje: <p className='text-red-500'>{detail.score}</p></h3>
+        : <h3 className='text-right mr-6'>Puntaje:<p className='text-green-500'>{detail.score}</p></h3>
+        }
+                           
+        </div> 
+        <Carousel />
+        </div>
+        
+        <br/>
+       
+             <div className=' mx-12'>
+              <p>
+                {detail.description}
+              </p>
+            </div> 
+           
+        <br/> 
+      
+      <div className='ml-12'> 
+        <p><strong>INCLUYE: </strong></p>
         {detail.included?.map((i,id) => (
-          <div key={id}>
-            <h4>{i.body}</h4>
+            <div key={id}>
+            <h4>{i}</h4>
             </div>
         ))}
-      <h3>{detail.score}</h3>
-      <h3>{detail.stock}</h3>
-      <h3>$ {detail.price}</h3>
-      {detail.comments?.map((c,id) => (
-        <li key={id}>
-          <br/>
-          {c}</li> 
-          ))}
-    </div>
+      </div>
+      <div className='text-right mr-12'>
+              <h3>Disponible: {detail.stock}</h3>
+              <h3>Precio: {detail.price}</h3>
+            </div>
+      
+      <br/>
+      <div className='mx-52 border-2 border-teal-600 mb-20 rounded-sm bg-teal-300'>
+      <p className='mt-3 ml-5'><strong>COMENTARIOS: </strong></p>
+        <CarouselCom/>
+      </div>
+      <div className='flex justify-end mr-2 mb-4'>
+              <Link to="/destination">
+                  <button>VOLVER</button>
+              </Link> 
+            </div>
+      </div>
+   
+  </div>
     )
 };
   export default Details;
