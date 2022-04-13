12ba0ff59a6fcc7b1504215836cc2bac9c1f2af0
Juth7 <mjudith079@gmail.com>
2022-04-13 18:23:33 -0400

    Validation Changes Login and Register

--- client/src/components/validate.jsx
+++ client/src/components/validate.jsx
@@ -1,6 +1,7 @@
 export const validate = (data) => {
   let errors = {};
-  let validateEmail = /\S+@\S+\.\S+/;
+  let validateEmail =
+    /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
   let validatePassword = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
   let validateUsername = /^[a-zA-Z]+(\S)*[a-zA-Z0-9]+.{1,20}$/;
   if (!data.email && !validateEmail.test(data.email)) {
@@ -11,9 +12,9 @@ export const validate = (data) => {
     errors.password =
       "La contraseña debe tener al menos 8 caracteres, una letra y un numero";
   }
-  if (!data.username) errors.username = "Ingrese un nombre de usuario";
-  if (!validateUsername.test(data.username)) {
-    errors.username = "El nombre de usuario debe tener al menos 3 caracteres";
+  if (!data.username && !validateUsername.test(data.username)) {
+    errors.username =
+      "Ingrese un nombre de usuario. Debe contener al menos 3 caracteres";
   }
   if (!data.title || data.title.length > 30)
     errors.title = "El titulo es obligatorio, y con un maximo de 30 letras";
