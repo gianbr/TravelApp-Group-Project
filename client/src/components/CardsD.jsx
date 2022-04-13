a5bf6d68fc313b3ad6c2b24cf3cafc1d43989e1b
Gian Brunner <gianbrunner1@gmail.com>
2022-04-13 16:24:04 -0300

    fixing card styles, standarized plains titles, add title in detail, input control in detail

--- client/src/components/CardsD.jsx
+++ client/src/components/CardsD.jsx
@@ -10,21 +10,21 @@ return(
                     // CARTA
     <div >
         <Link to={"/destination/" + id}>
-    <div className='h-full border-2 bg-gray-100 border-gray-100 border-opacity-100  overflow-hidden ' >
-            <img src={image} alt='vacation' className="h-96 w-96 rounded-lg shadow-lg shadow-[#040c16]"  />
-    <div className='px-6 py-4'>
-    <div>
-    <h2 className='text-base font-medium mb-1'>{title} </h2>
-    <p className='leading-relaxed mb-3'>{location} </p>
-    </div>
-    <div className='flex justify-between items-center'>
-                    <StarRating score={score} />
-       
+            <div className='h-full border-2 bg-gray-100 border-gray-100 border-opacity-100  overflow-hidden ' >
+                <div className="h-96 w-96 object-cover">
+                    <img src={image} alt='vacation' className="h-96 w-96 rounded-lg shadow-lg shadow-[#040c16]"  />
+                </div>
+                <div className='px-6 py-4'>
+                    <div>
+                        <h2 className='text-base font-medium mb-1'>{title} </h2>
+                        <p className='leading-relaxed mb-3'>{location} </p>
+                    </div>
+                    <div className='flex justify-between items-center'>
+                        <StarRating score={score} />
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
- 
-</Link>
+        </Link>
     </div> 
 )};
 export default CardsD;
--- client/src/components/Destino.jsx
+++ client/src/components/Destino.jsx
@@ -4,25 +4,26 @@ import { Link } from 'react-router-dom';
 
 const Destino = ({image, title, price, location,id }) => { 
     // CARTA DE LINK DESTINATION
-      return (
+    return (
                     // CARTA
-    <div className="rounded-lg">
-        <Link to={"/destination/" + id}>
-    <div className='w-96 border-2 bg-gray-100 border-opacity-100  overflow-hidden' style={{height:"100vh"}} >
-            <img src={image} alt='vacation' className="w-96 rounded-lg shadow-lg shadow-[#040c16]" style={{height:"70vh"}} />
-    <div className='px-6 py-4'>
-    <div>
-    <h2 className='text-base font-medium mb-1'>{title} </h2>
-    <p className='leading-relaxed mb-3'> <strong>Ubicación: {location} </strong> </p>
-    </div>
-    <div className='flex justify-between items-center'>
-               <p>  <strong>Precio: ${price}</strong> </p>
+        <div className="rounded-lg">
+            <Link to={"/destination/" + id}>
+            <div className='w-96 h-[556px] border-2 bg-gray-100 border-opacity-100  overflow-hidden' >  {/* h-[556px] es el tamaño de la card */}
+                <div className='w-96 h-96'> 
+                    <img src={image} alt='vacation' className="w-96 h-96 rounded-lg shadow-lg shadow-[#040c16]" />
+                </div>
+                <div className='px-6 py-4'>
+                    <div>
+                        <h2 className='text-base font-medium mb-1'>{title} </h2>
+                        <p className='leading-relaxed mb-3'> <strong>Ubicación: {location} </strong> </p>
+                    </div>
+                    <div className='flex justify-between items-center'>
+                    <p>  <strong>Precio: ${price}</strong> </p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
- 
-</Link>
-    </div> 
-      );
-  };
-  export default Destino;
+            </Link>
+        </div> 
+    );
+};
+export default Destino;
