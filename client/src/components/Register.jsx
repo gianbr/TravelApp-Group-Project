2032c922744ce0a661febc199eac136a5889fcfb
Juth7 <mjudith079@gmail.com>
2022-04-13 20:06:35 -0400

    Login and Register Validations

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,14 +1,14 @@
-import { React,useState } from 'react'
-import loginImg from '../assets/login.jpeg'
-import { signup } from '../actions'
-import { useDispatch,} from 'react-redux';
-import swal from 'sweetalert';
-import { validate } from './validate';
-import { Link } from 'react-router-dom';
-
+import { React, useState } from "react";
+import loginImg from "../assets/login.jpeg";
+import { signup } from "../actions";
+import { useDispatch } from "react-redux";
+import swal from "sweetalert";
+import { validate } from "./validate";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState({});
+  console.log(errors);
   const dispatch = useDispatch();
   const [data, setData] = useState({
     username: "",
@@ -17,66 +17,90 @@ const Register = () => {
   });
 
   const handleSubmit = async (e) => {
-    if(!data.email || !data.password || !data.username ){
-     return swal({title: "¡Rellene los campos para continuar!", icon: "error"});
-  }else { 
-    e.preventDefault();
-      dispatch(signup(data),
-      console.log("submit",data))
-    };
+    if (errors !== {}) {
+      e.preventDefault();
+      return swal({
+        title: "¡Rellene los campos para continuar!",
+        icon: "error",
+      });
+    } else {
+      e.preventDefault();
+      dispatch(signup(data), console.log("submit", data));
+    }
   };
 
-
-const handleChange = (e) => {
+  const handleChange = (e) => {
     setData({
-      ...data, 
-      [e.target.name] : e.target.value
-  });
-   setErrors(validate({
-       ...data, 
-       [e.target.name] : e.target.value
-  }));
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+    setErrors(
+      validate({
+        ...data,
+        [e.target.name]: e.target.value,
+      })
+    );
   };
-  
 
-return (
-  <div className='relative w-full h-screen bg-zinc-900/90'>
-    <img className='absolute w-full h-full object-cover mix-blend-overlay' src={loginImg} alt="/" />
-    <div className='flex flex-col justify-evenly items-center h-full'>
+  return (
+    <div className="relative w-full h-screen bg-zinc-900/90">
+      <img
+        className="absolute w-full h-full object-cover mix-blend-overlay"
+        src={loginImg}
+        alt="/"
+      />
+      <div className="flex flex-col justify-evenly items-center h-full">
+        <Link
+          to="/"
+          className="rounded-2xl py-2 p-3 focus:outline-none focus:ring focus:ring-indigo-500 bg-teal-400 hover:bg-indigo-500 relative text-white font-semibold"
+        >
+          <button>Inicio</button>
+        </Link>
 
-    <Link to='/' className='rounded-2xl py-2 p-3 focus:outline-none focus:ring focus:ring-indigo-500 bg-teal-400 hover:bg-indigo-500 relative text-white font-semibold'><button>Inicio</button></Link>
-    
-      <form className='max-w-[400px] w-full mx-auto bg-white p-8'>
-        <h2 className='text-4xl font-bold text-center py-4'>TRAVEL APP.</h2>
+        <form className="max-w-[400px] w-full mx-auto bg-white p-8">
+          <h2 className="text-4xl font-bold text-center py-4">TRAVEL APP.</h2>
 
-        <div className='flex flex-col mb-4'>
+          <div className="flex flex-col mb-4">
             <label>Usuario</label>
-            <input onChange={handleChange} className='border relative bg-gray-100 p-2' name='username' type="text" />
-            {errors.username && (<p>{errors.username}</p>)}
-            </div>
+            <input
+              onChange={handleChange}
+              className="border relative bg-gray-100 p-2"
+              name="username"
+              type="text"
+            />
+            {errors.username && <p>{errors.username}</p>}
+          </div>
 
-        <div className='flex flex-col mb-4'>
+          <div className="flex flex-col mb-4">
             <label>Correo</label>
-            <input onChange={handleChange} className='border relative bg-gray-100 p-2' name='email' type="text" />
-            {errors.email && (<p>{errors.email}</p>)}
-            </div>
-        
-        <div className='flex flex-col '>
+            <input
+              onChange={handleChange}
+              className="border relative bg-gray-100 p-2"
+              name="email"
+              type="text"
+            />
+            {errors.email && <p>{errors.email}</p>}
+          </div>
+
+          <div className="flex flex-col ">
             <label>Contraseña</label>
-            <input onChange={handleChange} className='border relative bg-gray-100 p-2' name='password'  type="password" />
-            {errors.password && (<p>{errors.password}</p>)}
-            </div>
-        <div onClick={handleSubmit} >
-        <button className='w-full py-3 mt-8 bg-teal-400 hover:bg-indigo-500 relative text-white'>Ingresa tus datos</button>
-          
-            </div>
-           
-            
-            </form>
-        </div>
+            <input
+              onChange={handleChange}
+              className="border relative bg-gray-100 p-2"
+              name="password"
+              type="password"
+            />
+            {errors.password && <p>{errors.password}</p>}
+          </div>
+          <div onClick={handleSubmit}>
+            <button className="w-full py-3 mt-8 bg-teal-400 hover:bg-indigo-500 relative text-white">
+              Ingresa tus datos
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
 
 export default Register;
--- client/src/components/validate.jsx
+++ client/src/components/validate.jsx
@@ -2,17 +2,16 @@ export const validate = (data) => {
   let errors = {};
   let validateEmail =
     /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
-  let validatePassword = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+  let validatePassword = /^(?=.*[A-Za-z])(?=.*\d).{8,15}$/;
   let validateUsername = /^[a-zA-Z]+(\S)*[a-zA-Z0-9]+.{1,20}$/;
-  if (!data.email && !validateEmail.test(data.email)) {
+  if (!data.email || !validateEmail.test(data.email)) {
     errors.email = "Ingrese un correo valido";
   }
-  if (!data.password) errors.password = "Ingrese una contraseña";
   if (!validatePassword.test(data.password)) {
     errors.password =
       "La contraseña debe tener al menos 8 caracteres, una letra y un numero";
   }
-  if (!data.username && !validateUsername.test(data.username)) {
+  if (!data.username || !validateUsername.test(data.username)) {
     errors.username =
       "Ingrese un nombre de usuario. Debe contener al menos 3 caracteres";
   }
